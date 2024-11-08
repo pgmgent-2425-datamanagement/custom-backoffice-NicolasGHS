9b1859fd05a1d4bf43ddc2f0442cee7d2ae7f549
NicolasGHS <nicoghyselincks@outlook.be>
2024-11-08 18:47:04 +0100

    show image in filemanager

--- views/filemanager.php
+++ views/filemanager.php
@@ -6,7 +6,11 @@ foreach($list as $item) :
         if(is_dir(BASE_DIR . '/public/images/' . $item)) : ?>
             <li><a href="/filemanager/<?= $item; ?>"><?= $item; ?></a></li>
         <?php else : ?>
-            <li><?= $item; ?> <a href="/filemanager/delete/<?= $item; ?>">delete</a></li>
+            <li>
+                <?= $item; ?> 
+                <img src="images/<?= $item; ?> " alt="Gitaar Foto" class="mt-4 max-w-xs">
+                <a href="/filemanager/delete/<?= $item; ?>">delete</a>
+            </li>
 <?php endif;
     endif;
 endforeach; ?>
